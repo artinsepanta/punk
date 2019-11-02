572788a32fb5d7b39ffdbcc442a2e2af8c7a4f41
artinsepanta <faribasalehi@hotmail.com>
2019-11-01 20:04:45 -0500

    punk

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component}  from 'react';
 import './App.css';
 
-function App() {
+
+ 
+class App extends Component {
+
+  state = {
+    beerList: [],
+    isLiked: [],
+  }
+
+  componentWillMount() {
+    this.fetchData();
+  }
+
+fetchData() {
+  const array1 =[];
+  fetch("https://api.punkapi.com/v2/beers")
+  .then(respond =>respond.json())
+  .then(data =>{
+    data.map(()=>{
+     return array1.push(0)
+    })
+    this.setState({
+      beerList : data,
+      isLiked : array1
+    })
+  })
+  .catch(error => console.log("oops, looks like we got an error: ", error));
+}
+
+handleClick = (index) =>{
+  const newArray = this.state.isLiked.map((item, newIndex) =>{
+    if(newIndex === index) {
+      if(item === 1){
+        return 0;
+      }else {
+        return item;
+      }
+    }
+    this.state({
+      isLiked : newArray
+    })
+  })
+}
+render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <ul>
+            {
+              this.state.beerList.map((obj, index) => {
+                const label = this.state.isLiked[index] === 1 ? 'Unlike' : 'Like'
+                return <li key={index}>
+                  <img src={obj.image_url} alt={obj.name} />
+                  <button onClick={() => this.handleClick(index)} value={this.state.isLiked}>
+                    {label}</button>
+                </li>
+              })
+            }
+          </ul>
+
+          }
+        
       </header>
     </div>
   );
 }
+
+}
+
 
 export default App;
